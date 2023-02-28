0e1b69431a3271f22e99c58c89ba50dd1803f96d
diegomated1 <diegocardwr@gmail.com>
2023-02-27 21:33:06 -0500

    created register route

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller.js";
-
+import authController from '../controllers/auth.controller.js';
 class UserRouter {
 
     router:Router;
@@ -11,11 +11,11 @@ class UserRouter {
     }
 
     private config(){
-        // GET users/:id_user      TRAE UN USUARIO CON SU ID
-        
-        this.router.route('/users/:id_user').get(userController.getById);
-        this.router.route('/auth').post(userController.auth);
-        this.router.route('/auth/login').post(userController.login)
+        this.router.route('/auth').post(authController.auth);
+        this.router.route('/auth/login').post(authController.login);
+        this.router.route('/auth/register').post(authController.register);
+
+        this.router.route('/user/:id_user').get(userController.getById);
         this.router.route('/user/:id_user').put(userController.modifyUsersById);
     }
 }
